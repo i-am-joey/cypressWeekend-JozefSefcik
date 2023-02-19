b3b7749a9f9c8fa746df25ba30659e9972a2c990
jozef.sefcik <jozef.sefcik@eset.com>
2023-02-19 16:09:12 +0100

    setup and load page

--- cypress/e2e/TaskOne.cy.js
+++ cypress/e2e/TaskOne.cy.js
@@ -1,5 +1,46 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+/// <reference types="cypress" />
+
+describe("Task One ", () => {
+
+  it("passes", () => {
+    cy.visit(Cypress.env("url") + "/en/country/france/")
+    cy.get('[data-test="CookiesPopup-Accept"]').click()
+  });
+
+  it("All sections are correctly loaded", () => {
+
   })
-})
+
+  it("Destination in search should be France", () => {
+
+  })
+
+  it("H1 element should have correct text", () => {
+
+  })
+
+  it("Click on first picture card in popular cities section", () => {
+
+  })
+
+  it("Verify rederiction to correct page in search/results", () => {
+
+  })
+
+  it("Add one additional luggage in filter", () => {
+
+  })
+
+  it("Verify that new results are loaded", () => {
+
+  })
+
+  it("Continue in reservation process from first choice", () => {
+
+  })
+
+  it("Verify redirection to booking page", () => {
+
+  })
+
+});
